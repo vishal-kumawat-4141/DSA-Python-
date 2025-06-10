9655e5f15ce86073460b6046630e0ee81a1625a4
Vishal Kumawat <vishalskumawat9@gmail.com>
2025-06-10 18:35:59 +0530

    palindrome check for string using recursion and while loop code added

--- Recurision/palindrome_recurision.py
+++ Recurision/palindrome_recurision.py
@@ -1,14 +1,14 @@
 # check string is palindrome or not
-s = input("Enter a string : ")
+# s = input("Enter a string : ")
 
 
-def palindrome(l=0, r=len(s) - 1):
-    global s
-    if l >= r:
-        return "string is palindrome"
-    elif s[l] != s[r]:
-        return "string is not palindrome"
-    return palindrome(l + 1, r - 1)
+# def palindrome(l=0, r=len(s) - 1):
+#     global s
+#     if l >= r:
+#         return "string is palindrome"
+#     elif s[l] != s[r]:
+#         return "string is not palindrome"
+#     return palindrome(l + 1, r - 1)
 
 
 # print(palindrome())
@@ -18,13 +18,18 @@ def palindrome(l=0, r=len(s) - 1):
 
 
 # using loop
-# s = input("enter the string :")
-# n = len(s)
-# l = 0
-# r = n - 1
-# while l < r:
-#     if s[l] != s[r]:
-#         print(f"{s} is not palindrome .")
-#     l += 1
-#     r -= 1
-# print(f"{s} is palindrome.")
+def palidrome():
+    s = input("enter the string :")
+    n = len(s)
+    l = 0
+    r = n - 1
+    while l < r:
+        if s[l] == s[r]:
+            l += 1
+            r -= 1
+        else:
+            return "string is not palindrome"
+    return "string is palindrome ."
+
+
+print(palidrome())
