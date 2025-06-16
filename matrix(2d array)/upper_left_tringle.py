ba2a43eedb0124f98f548a35d5478770ea091396
Vishal Kumawat <vishalskumawat9@gmail.com>
2025-06-16 15:35:45 +0530

    code added for upper left tringle

---
+++ matrix(2d array)/upper_left_tringle.py
@@ -0,0 +1,16 @@
+# make upper left tringle :---> * * *     _ * *      _ _ *
+l = [[1, 2, 4], [5, 7, 3], [8, 9, 6]]
+rows = len(l)
+columns = len(l[0])
+for i in range(0, rows):
+    for j in range(0, columns):
+        if i + j <= columns - 1:
+            print(l[i][j], end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+# output:-->
+# 1 2 4
+# 5 7
+# 8
