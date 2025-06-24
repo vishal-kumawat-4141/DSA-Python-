d203cbe42c1771e9e8f5f7b9a241a194bd65b10b
Vishal Kumawat <vishalskumawat9@gmail.com>
2025-06-24 15:25:37 +0530

    code added fro swapping two numbers usign xor operator

---
+++ bit manipulation(binary,decimal,oxta)/questions/swap_two_number.py
@@ -0,0 +1,7 @@
+# swapping two numbers using bit operators:--->
+a = 10
+b = 20
+# a = a ^ b
+# b = a ^ b   # (a^b)^b == a  because b ^ b = 0
+# a = a ^ b   # (a^b)^a == b  because a ^ a = 0
+print(f"a = {a} and b  = { b}")
