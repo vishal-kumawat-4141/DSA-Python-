ea32d0008e085fd36d7a197a6bc2aa101a5b1c49
Vishal Kumawat <vishalskumawat9@gmail.com>
2025-07-07 13:39:05 +0530

    code added for covert prefix to infix

---
+++ stack_&_Queue_implementation/prefix_infix_postfix/prefix_to_infix.py
@@ -0,0 +1,18 @@
+# convert the postfix to infix :---> this is start form last and the char between first pop and second pop element
+# T.C = O(N) , S.C = O(N)
+class Solution:
+    def postfix_to_Infix(self, s):
+        stack = []
+        for char in s[::-1]:
+            if char.isalnum():
+                stack.append(char)
+            else:
+                operand1 = stack.pop()
+                operand2 = stack.pop()
+                new_stack = f"({operand1}{char}{operand2})"
+                stack.append(new_stack)
+        return stack[-1]
+
+
+s1 = Solution()
+print(s1.postfix_to_Infix("*+PQ-MN"))
